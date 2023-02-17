ceec8e1506bf10fcfda1cdcebcf3ab2bc49c6384
Alexander Weichart <55558407+AlexW00@users.noreply.github.com>
2023-02-17 10:49:55 +0100

    impl: minimal example

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,17 @@
 import "./style.css";
+import cytoscape from "cytoscape";
+
 console.log("Hello World!");
+
+const app = document.getElementById("app");
+
+const elements = [
+	// List of graph-elements to start with (json)
+	// NODES
+	{ data: { id: "a" } }, // node a
+	{ data: { id: "b" } }, // node b
+	// EDGES
+	{ data: { id: "ab", source: "a", target: "b" } }, // edge ab
+];
+
+cytoscape({ elements: elements, container: app });
