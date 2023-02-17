a0eb209fad36ef89dd2f7bf52ee35d40feb441f8
Alexander Weichart <55558407+AlexW00@users.noreply.github.com>
2023-02-17 13:42:07 +0100

    added: eimi data json

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,9 @@
 import "./style.css";
 import cytoscape from "cytoscape";
+// import out.json from public folder
+import EIMI from "./data/eimi.json";
 
 console.log("Hello World!");
 
 const app = document.getElementById("app");
-
-const elements = [
-	// List of graph-elements to start with (json)
-	// NODES
-	{ data: { id: "a" } }, // node a
-	{ data: { id: "b" } }, // node b
-	// EDGES
-	{ data: { id: "ab", source: "a", target: "b" } }, // edge ab
-];
-
-cytoscape({ elements: elements, container: app });
+cytoscape({ elements: EIMI, container: app });
